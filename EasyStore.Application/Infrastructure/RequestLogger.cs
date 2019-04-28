be5dc3b464db62e5c6e818750a722607bc9d6a0e
Web Tech Media Group <WTMG@Thineskumar.local>
2019-04-28 22:19:05 +0200

    Request logger pipeline implemented

---
+++ EasyStore.Application/Infrastructure/RequestLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace EasyStore.Application.Infrastructure
+{
+    public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public RequestLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var name = typeof(TRequest).Name;
+
+            _logger.LogInformation("EasyStore Request: {Name} {@Request}", name, request);
+
+            return Task.CompletedTask;
+        }
+    }
+}
