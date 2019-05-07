423a66a71454496059d8689de75df0f5fe2c7170
Web Tech Media Group <WTMG@Thineskumar.local>
2019-05-07 23:36:27 +0200

    automapper, mediatr services resolved

--- EasyStore.WebAPI/Startup.cs
+++ EasyStore.WebAPI/Startup.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Reflection;
+using AutoMapper;
+using EasyStore.Application.Infrastructure;
+using EasyStore.Application.Infrastructure.AutoMapper;
+using EasyStore.Application.Interfaces;
+using EasyStore.Persistence;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,7 +27,25 @@ namespace EasyStore.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //Add AutoMapper
+            services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly });
+
+            //Add MediatR
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            //Add DbContext
+            services.AddDbContext<IEasyStoreDbContext, EasyStoreDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("EasyStoreDb")));
+
+
+
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                    //.AddFluentValidation(fv =>
+                    //{
+                        //fv.RegisterValidatorsFromAssemblyContaining<>();
+                    //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
