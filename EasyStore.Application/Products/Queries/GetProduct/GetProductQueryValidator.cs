bf92fbe7d3fbc05d2160c05d9ed4e0ff490f3455
Web Tech Media Group <WTMG@Thineskumar.local>
2019-05-13 21:04:58 +0200

    GetProductQuery validator added

---
+++ EasyStore.Application/Products/Queries/GetProduct/GetProductQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FluentValidation;
+
+namespace EasyStore.Application.Products.Queries.GetProduct
+{
+    public class GetProductQueryValidator : AbstractValidator<GetProductQuery>
+    {
+        public GetProductQueryValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty().WithMessage("Product Id is required to retrieve a product!");
+        }
+    }
+}
