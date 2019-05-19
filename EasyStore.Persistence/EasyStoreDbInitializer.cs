18a35c21ea3d24072791fb16246a58216bbf589f
Web Tech Media Group <WTMG@Thineskumar.local>
2019-05-19 20:05:38 +0200

    Initializer fix

--- EasyStore.Persistence/EasyStoreDbInitializer.cs
+++ EasyStore.Persistence/EasyStoreDbInitializer.cs
@@ -33,7 +33,7 @@ namespace EasyStore.Persistence
             {
                 new Product
                 {
-                    ProductId = 1,
+
                     ProductName = "Product1",
                     Price = new Money(100),
                     ProductDescription = "Description of the product 1"
@@ -41,7 +41,7 @@ namespace EasyStore.Persistence
                 },
                 new Product
                 {
-                    ProductId = 2,
+
                     ProductName = "Product2",
                     Price = new Money(200),
                     ProductDescription = "Description of the product 2"
@@ -49,7 +49,7 @@ namespace EasyStore.Persistence
                 },
                 new Product
                 {
-                    ProductId = 3,
+
                     ProductName = "Product3",
                     Price = new Money(300),
                     ProductDescription = "Description of the product 3"
@@ -57,7 +57,7 @@ namespace EasyStore.Persistence
                 },
                 new Product
                 {
-                    ProductId = 4,
+
                     ProductName = "Product4",
                     Price = new Money(400),
                     ProductDescription = "Description of the product 4"
@@ -76,21 +76,21 @@ namespace EasyStore.Persistence
             var customers = new[]
             {
                new Customer {
-                             CustomerId = 1, 
+                              
                              Address = new Address("Address 1", "Address 2", "Stockholm", "email@email.com", "Sweden", "14576"),
                              FirstName = "FirstName 1",
                              LastName = "LastName 2",
                              Phone = "0756353738"
                              },
                new Customer {
-                             CustomerId = 2,
+                             
                              Address = new Address("Address 2", "Address 3", "Oslo", "email1@email.com", "Norway", "14376"),
                              FirstName = "FirstName 2",
                              LastName = "LastName 3",
                              Phone = "0756353745"
                              },
                new Customer {
-                             CustomerId = 3,
+                             
                              Address = new Address("Address 3", "Address 4", "Washington", "email2@email.com", "USA", "198736"),
                              FirstName = "FirstName 4",
                              LastName = "LastName 5",
